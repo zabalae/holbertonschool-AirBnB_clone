c506d3615ec7af8e9e5e74a7b3c43e583fe47183
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-24 18:01:28 -0400

    fixing typo

--- models/base_model.py
+++ models/base_model.py
@@ -43,7 +43,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new()
+            storage.new(self)
 
     def __str__(self):
         '''Return a string representation of the object'''
