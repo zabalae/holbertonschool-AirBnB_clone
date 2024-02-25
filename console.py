90bb5e0d510e6f5273f54e3a3b58fb78fb583f90
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-02-24 21:08:23 -0400

    task 6

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+'''Contains the entry point of the command interpreter'''
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        '''Will exit the program'''
+        return True
+
+    def do_EOF(self, arg):
+        '''Will exit the program'''
+        print()
+        return True
+
+    def do_help(self, arg):
+        '''Will show help'''
+        super().do_help(arg)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
