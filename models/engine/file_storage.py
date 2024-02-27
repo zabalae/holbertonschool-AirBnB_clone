3e44aa1cfe296417d86307f64169176605cfb179
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-27 05:30:06 -0400

    Updating File Storage for task 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,11 @@ import json
 from os.path import isfile
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage:
