146aaf0fdd3320524c1253d41946b77c7f6529c5
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-29 09:25:01 -0400

    fixing test_user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -31,16 +31,19 @@ class TestUserClass(unittest.TestCase):
         self.assertTrue(issubclass(User, BaseModel))
 
     def test_user_firstname(self):
+        '''Test User first name'''
         u1 = User()
         u1.first_name = "Liz"
         self.assertEqual(u1.first_name, "Liz")
 
     def test_user_lastname(self):
+        ''''Test User last name'''
         u1 = User()
         u1.last_name = "Zabala"
         self.assertEqual(u1.last_name, "Zabala")
 
     def test_user_email(self):
+        '''Test User email'''
         u1 = User()
         u1.email = "Liz@example.com"
         self.assertEqual(u1.email, "Liz@example.com")
