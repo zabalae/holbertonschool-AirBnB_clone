2a04ed1ca17ec414cd53b27721e82b290a026ec1
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-01 18:35:17 -0400

    trying task 11

--- console.py
+++ console.py
@@ -95,17 +95,22 @@ class HBNBCommand(cmd.Cmd):
         '''Prints all str representation of all instances based or not
             on the class name
         '''
-        args = shlex.split(arg)
-        dict_t = storage.all()
-
-        if not args:
-            print([str(value) for value in dict_t.values()])
+        object_list = []
+        storage.reload()
+        objects = storage.all()
+        try:
+            if len(arg) != 0:
+                eval(arg)
+        except NameError:
+            print("** class doesn't exist **")
             return
-        if args[0] in self.classes:
-            print([str(value) for key, value in dict_t.items()
-                   if key.split(".")[0] == args[0]])
-            return
-        print("** class doesn't exist **")
+        for keys, value in objects.items():
+            if len(arg) != 0:
+                if type(value) is eval(arg):
+                    object_list.append(value.__str__())
+            else:
+                object_list.append(value.__str__())
+        print(object_list)
 
     def do_update(self, arg):
         '''Updates an instance based on the class name and id by adding
