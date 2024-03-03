956130f83d4bc5f04a88b161e6f66f9b34d43aff
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-03 11:38:41 -0400

    more test cases for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,7 +12,29 @@ class TestConsoleCommands(unittest.TestCase):
     def setUp(self):
         self.console = HBNBCommand()
 
-    def test_create_command(self):
+    def test_quit_command(self):
+        with self.assertRaises(SystemExit) as cm:
+            self.console.onecmd('quit')
+        self.assertEqual(cm.exception.code, True)
+
+    def test_eof_command(self):
+        with self.assertRaises(SystemExit) as cm:
+            self.console.onecmd('EOF')
+        self.assertEqual(cm.exception.code, True)
+
+    def test_help_command(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.console.onecmd('help')
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue("List available commands" in output)
+
+    def test_empty_line_command(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.console.onecmd('')
+            output = mock_stdout.getvalue().strip()
+            self.assertEqual(output, '')
+
+    def test_create_base_model_command(self):
         with patch('sys.stdout', new=StringIO()) as mock_stdout:
             self.console.onecmd('create BaseModel')
             output = mock_stdout.getvalue().strip()
@@ -36,9 +58,9 @@ class TestConsoleCommands(unittest.TestCase):
             output = mock_stdout.getvalue().strip()
             self.assertEqual(output, "[]")
 
-    def test_update_command(self):
+    def test_base_model_update_with_dict_command(self):
         with patch('sys.stdout', new=StringIO()) as mock_stdout:
-            self.console.onecmd('update BaseModel 123 attribute value')
+            self.console.onecmd("update BaseModel 123 {'attribute_name': 'string_value'}")
             output = mock_stdout.getvalue().strip()
             self.assertEqual(output, "** no instance found **")
 
